b831f5c57b576db4f99b3fda88898d24e692dad1
Cristian Morataya <cris.morataya@gmail.com>
2016-10-20 22:37:31 -0600

    Improve the handlig of the animation of the commensal

--- scripts/TableOne.gd
+++ scripts/TableOne.gd
@@ -1,7 +1,8 @@
 extends Container
+var play = false
+var is_playing = false
 
 func _ready():
-	get_node("Eating/anim").play("anim")
 	set_process(true)
 
 func _process(delta):
@@ -11,8 +12,18 @@ func _process(delta):
 	var fourStatus = get_node("ChairFour")
 	var fiveStatus = get_node("ChairFive")
 	var eatSprite = get_node("Eating")
-	
+
+	if (play):
+		if (!is_playing):
+			get_node("Eating/anim").play("anim")
+			is_playing = true
+	else:
+		get_node("Eating/anim").stop()
+
 	if (oneStatus.is_pressed() && twoStatus.is_pressed() && threeStatus.is_pressed() && fourStatus.is_pressed() && fiveStatus.is_pressed() ):
 		eatSprite.show()
+		play = true
 	elif (!oneStatus.is_pressed() && !twoStatus.is_pressed() && !threeStatus.is_pressed() && !fourStatus.is_pressed() && !fiveStatus.is_pressed() ):
 		eatSprite.hide()
+		play = false
+		is_playing = false
